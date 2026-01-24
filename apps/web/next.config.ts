64ec782ecbdf7d76b6a716a15836e60eb0eabff1
rafia <akmalrafi1132@gmail.com>
2026-01-24 15:41:57 +0700

    update next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: ".next",
+  basePath: "",
+  output: "standalone",
 };
 
 export default nextConfig;
